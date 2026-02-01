9284322a1b3f376482dcd37f6b8c7548d7465df9
Akhileswar Kamale <akhilkamale@gmail.com>
2026-02-01 22:35:12 +0530

    feat: solved valid_anagram problem

---
+++ Strings/242_valid_anagram.py
@@ -0,0 +1,17 @@
+def isAnagram(s,t):
+    if len(s) != len(t):
+        return False
+    
+    char_count = {}
+
+    for char in s:
+        char_count[char] = char_count.get(char,0) + 1
+
+    for char in t:
+        if char not in char_count or char_count == 0:
+            return False
+        char_count[char] -= 1
+
+    return True
+
+print(isAnagram("anagram","nagaram"))
