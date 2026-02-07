d79d2b13a51663106afbca08636df998ffe1e70a
Akhileswar Kamale <akhilkamale@gmail.com>
2026-02-07 16:57:35 +0530

    feat: Add Two Sum II

--- Arrays/167_two_sum_II.py
+++ Arrays/167_two_sum_II.py
@@ -5,12 +5,12 @@ def twoSum(nums, target):
     while left < right:
         s = nums[left] + nums[right]
 
-        if s == target:
-            return [left + 1, right + 1]
-        elif s < target:
+        if s < target:
             left += 1
-        else:
+        elif s > target:
             right -= 1
+        else:
+            return [left + 1, right + 1]
 
 print(twoSum([2,7,11,15],9))
 
