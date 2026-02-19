16884bc8672115b37893a78de9ad74fd90721031
Akhileswar Kamale <akhilkamale@gmail.com>
2026-02-19 22:46:16 +0530

    feat: Add Sorted squares problem

--- Arrays/977_sorted_squares.py
+++ Arrays/977_sorted_squares.py
@@ -1,17 +1,22 @@
 def sortedSquares(nums):
     n = len(nums)
-    res = [0] * n
-    left, right = 0, n-1
+    result = [0] * n
 
+    left, right = 0, n - 1
     pos = n - 1
+
     while left <= right:
         if abs(nums[left]) > abs(nums[right]):
-            res[pos] = nums[left] ** 2
+            result[pos] = nums[left] ** 2
             left += 1
         else:
-            res[pos] = nums[right] **2 
+            result[pos] = nums[right] ** 2
             right -= 1
+
         pos -= 1
-    return res
+
+    return result
+    
+    
 
 print(sortedSquares([-4,-1,0,3,10]))  
