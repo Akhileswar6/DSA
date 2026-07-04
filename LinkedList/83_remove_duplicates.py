d2346f8c8ef5cf45d00299b546cff8fcc701387a
Akhileswar Kamale <akhilkamale@gmail.com>
2026-07-04 17:01:20 +0530

    feat: add rem duplicates & merge linkedlist sol

---
+++ LinkedList/83_remove_duplicates.py
@@ -0,0 +1,19 @@
+# Input  = 1 -> 1 -> 2 -> 3 -> 3
+# Output = 1 -> 2 -> 3
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head):
+        curr = head
+
+        while curr and curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return head
+        
