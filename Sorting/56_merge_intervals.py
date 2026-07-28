88e0f0739c455272ab2b20f9727d043f8aa9ba6c
Akhileswar Kamale <akhilkamale@gmail.com>
2026-07-28 23:50:46 +0530

    feat: add merge & insert intervals code

---
+++ Sorting/56_merge_intervals.py
@@ -0,0 +1,13 @@
+def merge(intervals):
+    intervals.sort()
+    merged = []
+
+    for interval in intervals:
+        if not merged or interval[0] > merged[-1][1]:
+            merged.append(interval)
+        else:
+            merged[-1][1] = max(merged[-1][1], interval[1])
+
+    return merged
+
+print(merge([[1,3],[2,6],[8,10],[15,18]]))
