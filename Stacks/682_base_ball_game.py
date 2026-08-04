7fcf9b7d14bfeae2f52afb4714b2e6516efc4cb4
Akhileswar Kamale <akhilkamale@gmail.com>
2026-08-04 15:49:21 +0530

    feat: added Valid-parentheses & base-ball

---
+++ Stacks/682_base_ball_game.py
@@ -0,0 +1,20 @@
+def calPoints(operations):
+    stack = []
+
+    for op in operations:
+        if op == "+":
+            stack.append(stack[-1] + stack[-2])
+        elif op == "D":
+            stack.append(2 * stack[-1])
+        elif op == "C":
+            stack.pop()
+        else:
+            stack.append(int(op))
+
+    res = 0
+    for i in stack:
+        res += i
+
+    return res
+
+print(calPoints(["5","2","C","D","+"]))
