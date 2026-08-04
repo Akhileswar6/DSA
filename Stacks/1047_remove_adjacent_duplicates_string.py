c7cb8694ee712195de16476a59af618bfdf71514
Akhileswar Kamale <akhilkamale@gmail.com>
2026-08-04 15:50:46 +0530

    feat: add str-compare & rem adjacent dups

---
+++ Stacks/1047_remove_adjacent_duplicates_string.py
@@ -0,0 +1,11 @@
+def removeAdjacent(s):
+    stack = []
+    for ch in s:
+        if stack and stack[-1] == ch:
+            stack.pop()
+        else:
+            stack.append(ch)
+
+    return "".join(stack)
+
+print(removeAdjacent("abbaca"))
---
+++ Stacks/844_backspace_string_compare.py
@@ -0,0 +1,15 @@
+def backSpaceCompare(s, t):
+    def build(string):
+        stack = []
+        for ch in string:
+            if ch != "#":
+                stack.append(ch)
+            elif stack:
+                stack.pop()
+
+        return "".join(stack)
+
+    return build(s) == build(t)
+
+
+print(backSpaceCompare('ab#c', "ad#c"))
