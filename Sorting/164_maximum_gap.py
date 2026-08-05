d452b74869e465189c21beac273d9e5065182353
Akhileswar Kamale <akhilkamale@gmail.com>
2026-08-05 23:32:37 +0530

    feat: add max-gap code

---
+++ Sorting/164_maximum_gap.py
@@ -0,0 +1,14 @@
+def maximumGap(nums):
+    if len(nums) < 2:
+        return 0
+
+    nums.sort()
+
+    maximum_gap = 0
+
+    for i in range(1, len(nums)):
+        maximum_gap = max(maximum_gap, nums[i] - nums[i-1])
+
+    return maximum_gap
+
+print(maximumGap([3,9,12,1]))
