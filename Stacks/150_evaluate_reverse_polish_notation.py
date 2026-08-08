4c090b377ebfd3417c551e0fd5fca4cf537d39d2
Akhileswar Kamale <akhilkamale@gmail.com>
2026-08-08 23:27:30 +0530

    feat: add eval-reverse-polish & simplify-path code

---
+++ Stacks/150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,31 @@
+def evalRPN(tokens):
+    stack = []
+
+    for token in tokens:
+        if token == "+":
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(a + b)
+
+        elif token == "-":
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(a - b)
+
+        elif token == "*":
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(a * b)
+
+        elif token == "/":
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(int(a / b))
+
+
+        else:
+            stack.append(int(token))
+
+    return stack[0]
+
+print(evalRPN(["2","1","+","3","*"]))
